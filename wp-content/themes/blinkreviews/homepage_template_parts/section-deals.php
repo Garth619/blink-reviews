8b9fdccfbbca9fa131a9e562fa044eb5a7617d09
Garrett Cullen <Garth619@users.noreply.github.com>
2017-12-20 21:39:14 -0800

    push

--- wp-content/themes/blinkreviews/homepage_template_parts/section-deals.php
+++ wp-content/themes/blinkreviews/homepage_template_parts/section-deals.php
@@ -24,26 +24,77 @@
 				
 				<div class="redeem_wrapper">
 					
+					
 					<div class="menu_item">
 						
-						<div class="menu_item_title">Brow wax</div><!-- menu_item_title -->
+						<div class="menu_item_top">
+						
+						<div class="menu_item_title">
+							
+							<span class="large_header">Brow Wax</span><!-- large_header -->
+							
+							</div><!-- menu_item_title -->
+						
+						<span class="white_line"></span><!-- white_line -->
 						
 						<div class="menu_item_price_wrapper">
 							
+							<div class="mobile_view">
+							
+							<img class="tag_icon" src="<?php bloginfo('template_directory');?>/images/tag.svg"/>
+							
 							<span class="coupon_price">$5 off</span><!-- coupon_price -->
+							
+							</div><!-- mobile_view -->
 							
 							<span class="redeem_button">Redeem</span><!-- redeem_button -->
 							
 						</div><!-- menu_item_price_wrapper -->
 						
+						</div><!-- menu_item_top -->
+						
+						
+					</div><!-- menu_item -->
+
+					
+					<div class="menu_item">
+						
+						<div class="menu_item_top">
+						
+						<div class="menu_item_title">
+							
+							<span class="large_header">Blink Trio Brush Set</span><!-- large_header -->
+							
+							</div><!-- menu_item_title -->
+						
+						<span class="white_line"></span><!-- white_line -->
+						
+						<div class="menu_item_price_wrapper">
+							
+							<div class="mobile_view">
+							
+							<img class="tag_icon" src="<?php bloginfo('template_directory');?>/images/tag.svg"/>
+							
+							<span class="coupon_price">15% off</span><!-- coupon_price -->
+							
+							</div><!-- mobile_view -->
+							
+							<span class="redeem_button">Redeem</span><!-- redeem_button -->
+							
+						</div><!-- menu_item_price_wrapper -->
+						
+						</div><!-- menu_item_top -->
+						
+						<div class="menu_item_bottom">
+							
+							<span class="small_header">(cleansing brush, eyeliner brush and grooming brush)<br/>regular price $30</span><!-- small_header -->
+							
+						</div><!-- menu_item_bottom -->
+						
 					</div><!-- menu_item -->
 					
 					
 				</div><!-- redeem_wrapper -->
-				
-				
-				
-				<span>15% off the Blink trio brush set<br/>(cleansing brush, eyeliner brush and grooming brush)<br/>regular price $30 for the set of 3</span>
 				
 				
 			</div><!-- deals_content_inner -->
